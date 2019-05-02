70212158d302235cbe5bec6d25226caa2e4e0191
nakaded <nakaded1018@gamail.com>
2019-05-02 15:09:32 +0900

    page add

--- tk_wp0502/index.php
+++ tk_wp0502/index.php
@@ -1,2 +1,15 @@
 <?php get_header(); ?>
+<ul style="display:flex; text-align:center;margin:40px auto;">
+    <li>TOP</li>
+    <li>collection</li>
+    <li>works</li>
+    <li>about</li>
+    <li>shop</li>
+    <li>contact</li>
+</ul>
+
+<p>mv</p>
+
+<p>collection</p>
+
 <?php get_footer(); ?>
---
+++ tk_wp0502/page.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+/**
+  *Template Name: collection
+  */
+<section>
+        <div class="flex">
+            <div class="l-content">
+                <img src="<?php echo get_stylesheet_directory_uri(); ?>./img/test01.jpg" alt="hoge" />
+            </div>
+            <div class="r-content">
+                <p>test test test test test test test test test test test test test test test </p>
+            </div>
+        </div>
+</section>
+<?php get_footer(); ?>
