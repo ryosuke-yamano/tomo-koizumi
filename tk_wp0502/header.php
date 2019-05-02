1f320da53897610cf88798aa11077c5959e01d16
山野良介 <ryosukeyamano@suganoMacBook.local>
2019-05-02 14:29:22 +0900

    add: index.php

--- tk_wp0502/header.php
+++ tk_wp0502/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <?php wp_heade(); ?>
+    <?php wp_head(); ?>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
